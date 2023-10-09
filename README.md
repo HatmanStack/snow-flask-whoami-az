002c0e66fbbe39da046e6f6fb1b0cc343c1cc3ce
hatmanstack <hatmanstack@protonmail.com>
2023-10-09 13:23:02 -0500

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# snow-flask-whoami-az
+# Who Am I : AZ
+
+This is the Azure version of Who Am I.  It uses Azure Functions to build a microservice with Flask that provides an API for querying and posting information to a Snowflake Database.  The information is displayed with interactive Vega-Lite visualizations.
+
+## Changes
+
+The service is built and deployed using VSCode. When using Azure Functions it's necessary to rename your entrypoint to function_app.py and flatten the directory structure.  Rendering templates doesn't work off the shelf in Azure Functions.  The workaround is to read the .html as a string replacing the relevant parts.
+
+## Local Development
+
+To run locally:
+
+Running Azure functions locally is not straightforward.  It's necessary to have the vscode extension, Azurite, installed to simulate azure cloud storage. Microsoft learn is great [here]("https://learn.microsoft.com/en-us/azure/azure-functions/functions-develop-vs-code"). 
+
+Remember to replace `REGION`, `USER`, `PASSWORD` with your own Snowflake credentials and match the Schema to the one provided. You can add these env variables in the configuration once the function is deployed. 
+
+
---
+++ function_app.py
@@ -0,0 +1,70 @@
+import azure.functions as func 
+from flask import Flask, Response, request
+from snowflake import connector
+import pandas as pd
+import os
+import pathlib
+
+flask_app = Flask(__name__)
+
+@flask_app.route('/Home')
+def homepage():
+    cur = cnx.cursor().execute("Select Name, count(*) from ADDRESSES group by NAME")
+    data4charts=pd.DataFrame(cur.fetchall(), columns=['NAME','vote'])
+    data4chartsJSON = data4charts.to_json(orient='records')
+    html_file = pathlib.Path(__file__).parent / "home.html"
+    htmlString = html_file.read_text()
+    returnString = htmlString.replace("****data4chartsJSON****", data4chartsJSON)
+    return Response(returnString)
+    
+@flask_app.route('/Submit')
+def submitpage():
+    html_file = pathlib.Path(__file__).parent / "submit.html"
+    htmlString = html_file.read_text()
+    return Response(htmlString)
+
+@flask_app.route('/HardData')
+def hardData():
+    html_file = pathlib.Path(__file__).parent / "harddata.html"
+    htmlString = html_file.read_text()
+    dfhtml = updateRows().to_html()
+    htmlString = htmlString.replace('****dfhtml****', dfhtml)
+    return Response(htmlString)
+
+@flask_app.route('/Thanks', methods=["POST"])
+def thanks4submit():
+    address = request.form.get("cname")
+    name = request.form.get("uname")
+    insertRow(address, name)
+    html_file = pathlib.Path(__file__).parent / "thanks.html"
+    htmlString = html_file.read_text()
+    return Response(htmlString)
+  
+#snowflake
+cnx = connector.connect(
+    account= os.getenv('REGION'),
+    user= os.getenv('USER'),
+    password= os.getenv('PASSWORD'),
+    warehouse='COMPUTE_WH',
+    database='DEMO_DB',
+    schema='PUBLIC',
+    role='ACCOUNTADMIN'
+)
+
+def insertRow(address, name):
+    cur = cnx.cursor()
+    updateString = "INSERT INTO ADDRESSES(ADDRESS, NAME) VALUES ('{}', '{}')".format(address, name)
+    cur.execute(updateString)
+
+def updateRows():
+    cur = cnx.cursor()
+    cur.execute("SELECT * FROM ADDRESSES")
+    rows = pd.DataFrame(cur.fetchall(),columns=['ADDRESS', 'NAME'])
+    return rows
+
+
+app = func.WsgiFunctionApp(app=flask_app.wsgi_app, 
+                           http_auth_level=func.AuthLevel.ANONYMOUS) 
+
+
+
